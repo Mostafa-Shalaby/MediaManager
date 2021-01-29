ff63fd1bed640b83419177e92de173c38eb39356
Mostafa Shalaby <mostafa.shalaby98@gmail.com>
2021-01-29 19:04:49 +0200

    Adds implementations for RelayCommand.
    - Non-parametered type relaycommand.
    - A generic type relaycommand<t> that allow a a typed parameter to pass for execution.

---
+++ MediaManager.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MediaManager.WPF.Commands
+{
+    /// <summary>
+    /// RelayCommand that can run a non parametered method.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action execute, Predicate<Object> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ MediaManager.WPF/Commands/RelayCommand{T}.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MediaManager.WPF.Commands
+{
+    /// <summary>
+    /// RelayCommand that can run a method with a generic typed parameter passed.
+    /// </summary>
+    /// <typeparam name="T">Type of the Parameterto be Passed</typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+
+
+        public RelayCommand(Action<T> execute, Predicate<Object> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
